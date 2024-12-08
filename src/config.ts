10820eb5b1b6b9dbf3aa8ad3f3251e3bf10f90ed
Mai Tuyen <maituyen4496@gmail.com>
2024-12-08 14:20:43 +0700

    add http client

---
+++ src/config.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+const configSchema = z.object({
+  NEXT_PUBLIC_API_ENDPOINT: z.string(),
+  NEXT_PUBLIC_URL: z.string(),
+});
+
+const configProject = configSchema.safeParse({
+  // validate object và trả về giá trị nếu pass
+  NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
+  NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_API_ENDPOINT,
+});
+
+if (!configProject.success) {
+  console.error(configProject.error.errors);
+  throw new Error("Invalid env");
+}
+
+const envConfig = configProject.data;
+
+export default envConfig;
